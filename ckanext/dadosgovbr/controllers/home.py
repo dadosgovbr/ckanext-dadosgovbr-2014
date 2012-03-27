4b88e4ecab1b98ba3be3a9eed8c9e1033abec90c
Augusto Herrmann <augusto.herrmann@planejamento.gov.br>
2012-03-27 16:23:04 -0300

    criada a parte de noticias no controlador da home

--- ckanext/dadosgovbr/controllers/home.py
+++ ckanext/dadosgovbr/controllers/home.py
@@ -1,7 +1,30 @@
+from ckan.lib.base import c, h
 from ckan.controllers.home import HomeController
+
+from feedreader.parser import from_url
 
 class DadosGovBrHomeController(HomeController):
     """dados.gov.br theme customized home controller
     """
+    @staticmethod
+    def limita_tamanho(s, tamanho):
+        s = unicode(s)
+        return s if (len(s) < tamanho) else s[:(tamanho - 5)].rsplit(u" ", 1)[0] + u" &hellip;"
+    
     def index(self):
+        """This handles dados.gov.br's index home page.
+        All extra data displayed on the home page should be handled here.
+        """
+        # featured datasets section, read from a specific dataset group
+        
+        # news section, parsed from feed
+        parsed = from_url('http://189.9.137.65/wp/index.php/feed')
+        c.articles = []
+        for entry in parsed.entries:
+            c.articles += (entry.link, limita_tamanho(entry.title, 70), limita_tamanho(entry.description, 165))
+        
+        # most recent datasets section
+        
+        # most viewed datasets section, from ckanext-googleanalytics
+        
         return super(DadosGovBrHomeController).index()
