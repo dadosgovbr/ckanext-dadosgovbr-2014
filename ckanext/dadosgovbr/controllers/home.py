314962bc41eac0b6e60447160f70f98f2d80e4a7
Augusto Herrmann <augusto.herrmann@planejamento.gov.br>
2012-03-28 13:58:56 -0300

    iniciada a implementação das etiquetas mais usadas na página principal

--- ckanext/dadosgovbr/controllers/home.py
+++ ckanext/dadosgovbr/controllers/home.py
@@ -1,15 +1,16 @@
 from ckan.lib.base import c, h
 from ckan.controllers.home import HomeController
 
-
-
 class DadosGovBrHomeController(HomeController):
     """dados.gov.br theme customized home controller
     """
+    
+    # auxiliary static methods
     @staticmethod
     def limita_tamanho(s, tamanho):
         s = unicode(s)
         return s if (len(s) < tamanho) else s[:(tamanho - 5)].rsplit(u" ", 1)[0] + u" ..."
+    
     @staticmethod
     def formata_data(d):
         return d.strftime("%d/%m/%Y")
@@ -39,7 +40,22 @@ class DadosGovBrHomeController(HomeController):
 
         # Enable to set resources variable, don't forget to enable it on template too!
         #c.top_resources = dbutil.get_top_resources(limit=10)
-
+    
+    @staticmethod
+    def set_top_tags():
+        """Sets the c.top_tags variable for a template to render the
+        most used tags.
+        """
+        from ckan.logic import get_action
+        
+        tag_limit = 40
+        
+        data_dict = {}        
+        data_dict['limit'] = tag_limit
+        data_dict['return_objects'] = True
+        results = get_action('tag_list')(context,data_dict)
+        c.top_tags = results
+    
     def index(self):
         """This handles dados.gov.br's index home page.
         All extra data displayed on the home page should be handled here.
@@ -54,5 +70,6 @@ class DadosGovBrHomeController(HomeController):
         # most viewed datasets section, from ckanext-googleanalytics
         self.set_most_viewed_datasets()
 
-
+        # top tags section
+        self.set_top_tags()
         return super(DadosGovBrHomeController, self).index()
