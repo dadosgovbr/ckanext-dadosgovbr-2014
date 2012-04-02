24a6d023eb0e9cdc8cd8e674201a0656756ebdb8
Augusto Herrmann <augusto.herrmann@planejamento.gov.br>
2012-04-02 17:58:17 -0300

    trazendo o código do PackageController existente na versão instalada do CKAN

--- ckanext/dadosgovbr/controllers/package.py
+++ ckanext/dadosgovbr/controllers/package.py
@@ -6,20 +6,7 @@ class DadosGovBrDatasetController(PackageController):
     """A customized controller for the dataset related views on the
     ckanext-dadosgovbr theme.
     """
-    def read(self, id, format='html'):
-        # Check we know the content type, if not then it is likely a revision
-        # and therefore we should merge the format onto the end of id
-        ctype,extension,loader = self._content_type_for_format(format)
-        if not ctype:
-            # Reconstitute the ID if we don't know what content type to use
-            ctype = "text/html; charset=utf-8"
-            id = "%s.%s" % (id, format)
-            format = 'html'
-        else:
-            format = extension
-
-        response.headers['Content-Type'] = ctype
-
+    def read(self, id):
         package_type = self._get_package_type(id.split('@')[0])
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'extras_as_string': True,
@@ -42,17 +29,21 @@ class DadosGovBrDatasetController(PackageController):
                     abort(400, _('Invalid revision format: %r') % e.args)
         elif len(split) > 2:
             abort(400, _('Invalid revision format: %r') % 'Too many "@" symbols')
-
+            
         #check if package exists
         try:
             c.pkg_dict = get_action('package_show')(context, data_dict)
             c.pkg = context['package']
-            c.resources_json = json.dumps(c.pkg_dict.get('resources',[]))
+            c.pkg_json = json.dumps(c.pkg_dict)
         except NotFound:
             abort(404, _('Dataset not found'))
         except NotAuthorized:
             abort(401, _('Unauthorized to read package %s') % id)
-
+        
+        #set a cookie so we know whether to display the welcome message
+        c.hide_welcome_message = bool(request.cookies.get('hide_welcome_message', False))
+        response.set_cookie('hide_welcome_message', '1', max_age=3600) #(make cross-site?)
+        
         # used by dadosgovbr_dataset plugin
         import re
         extras = c.pkg.extras
@@ -77,10 +68,14 @@ class DadosGovBrDatasetController(PackageController):
                 ckan.logic.action.get.package_activity_list_html(context,
                     {'id': c.current_package_id})
 
+        if config.get('rdf_packages'):
+            accept_header = request.headers.get('Accept', '*/*')
+            for content_type, exts in negotiate(autoneg_cfg, accept_header):
+                if "html" not in exts: 
+                    rdf_url = '%s%s.%s' % (config['rdf_packages'], c.pkg.id, exts[0])
+                    redirect(rdf_url, code=303)
+                break
+
         PackageSaver().render_package(c.pkg_dict, context)
-
-        template = self._read_template( package_type )
-        template = template[:template.index('.')+1] + format
-
-        return render( template, loader_class=loader)
+        return render('package/read.html')
 
