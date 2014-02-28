ed4a515d0bb977199b0c5cf3fe703bc0921aa38b
Christian Miranda <christian.miranda@planejamento.gov.br>
2014-02-28 10:33:05 -0300

    Modificando o metodo read para a versao 2.2 do CKAN

--- ckanext/dadosgovbr/controllers/package.py
+++ ckanext/dadosgovbr/controllers/package.py
@@ -8,7 +8,7 @@ from ckan.lib.helpers import date_str_to_datetime
 from ckan.logic import NotFound, NotAuthorized
 from ckan.lib.base import request, _, c, BaseController, model, abort, h, g, render
 from ckan.lib.base import response, redirect, gettext
-from ckan.lib.package_saver import PackageSaver
+import ckan.lib.package_saver as package_saver
 from ckan.lib.helpers import json
 import ckan.logic.action.get
 
@@ -19,24 +19,25 @@ class DadosGovBrDatasetController(PackageController):
     ckanext-dadosgovbr theme.
     """
     def read(self, id, format='html'):
-        # Check we know the content type, if not then it is likely a revision
-        # and therefore we should merge the format onto the end of id
-        #ctype,extension,loader = self._content_type_for_format(format)
-        ctype = extension = None
-        if not ctype:
-            # Reconstitute the ID if we don't know what content type to use
-            ctype = "text/html; charset=utf-8"
-            id = "%s.%s" % (id, format)
-            format = 'html'
+
+        if not format == 'html':
+            ctype, extension, loader = \
+                self._content_type_from_extension(format)
+            if not ctype:
+                # An unknown format, we'll carry on in case it is a
+                # revision specifier and re-constitute the original id
+                id = "%s.%s" % (id, format)
+                ctype, format, loader = "text/html; charset=utf-8", "html", \
+                    MarkupTemplate
         else:
-            format = extension
+            ctype, format, loader = self._content_type_from_accept()
 
         response.headers['Content-Type'] = ctype
 
         package_type = self._get_package_type(id.split('@')[0])
         context = {'model': model, 'session': model.Session,
-                   'user': c.user or c.author, 'extras_as_string': True,
-                   'for_view': True}
+                   'user': c.user or c.author, 'for_view': True,
+                   'auth_user_obj': c.userobj}
         data_dict = {'id': id}
 
         # interpret @<revision_id> or @<date> suffix
@@ -47,25 +48,25 @@ class DadosGovBrDatasetController(PackageController):
                 context['revision_id'] = revision_ref
             else:
                 try:
-                    date = date_str_to_datetime(revision_ref)
+                    date = h.date_str_to_datetime(revision_ref)
                     context['revision_date'] = date
                 except TypeError, e:
                     abort(400, _('Invalid revision format: %r') % e.args)
                 except ValueError, e:
                     abort(400, _('Invalid revision format: %r') % e.args)
         elif len(split) > 2:
-            abort(400, _('Invalid revision format: %r') % 'Too many "@" symbols')
+            abort(400, _('Invalid revision format: %r') %
+                  'Too many "@" symbols')
 
-        #check if package exists
+        # check if package exists
         try:
             c.pkg_dict = get_action('package_show')(context, data_dict)
             c.pkg = context['package']
-            c.resources_json = json.dumps(c.pkg_dict.get('resources',[]))
         except NotFound:
             abort(404, _('Dataset not found'))
         except NotAuthorized:
             abort(401, _('Unauthorized to read package %s') % id)
-        
+
         # used by dadosgovbr_dataset plugin
         import re
         extras = c.pkg.extras
@@ -82,20 +83,28 @@ class DadosGovBrDatasetController(PackageController):
         
         # used by disqus plugin
         c.current_package_id = c.pkg.id
-        c.related_count = len(c.pkg.related)
+        c.related_count = c.pkg.related_count
 
-        # Add the package's activity stream (already rendered to HTML) to the
-        # template context for the package/read.html template to retrieve
-        # later.
-        c.package_activity_stream = \
-                ckan.logic.action.get.package_activity_list_html(context,
-                    {'id': c.current_package_id})
+        # can the resources be previewed?
+        for resource in c.pkg_dict['resources']:
+            resource['can_be_previewed'] = self._resource_preview(
+                {'resource': resource, 'package': c.pkg_dict})
 
-        PackageSaver().render_package(c.pkg_dict, context)
+        self._setup_template_variables(context, {'id': id},
+                                       package_type=package_type)
 
-        template = self._read_template( package_type )
-        template = template[:template.index('.')+1] + format
+        package_saver.PackageSaver().render_package(c.pkg_dict, context)
 
-        #return render( template, loader_class=loader)
-        return render( template)
+        template = self._read_template(package_type)
+        template = template[:template.index('.') + 1] + format
 
+        try:
+            return render(template, loader_class=loader)
+        except ckan.lib.render.TemplateNotFound:
+            msg = _("Viewing {package_type} datasets in {format} format is "
+                    "not supported (template file {file} not found).".format(
+                    package_type=package_type, format=format, file=template))
+            abort(404, msg)
+
+        assert False, "We should never get here"
+        
